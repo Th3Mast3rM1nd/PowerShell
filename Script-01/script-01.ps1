c38f9b2aa91a7c65752f9a7e9d0e8745e2397661
Th3Mast3rM1nd <92652606+Th3Mast3rM1nd@users.noreply.github.com>
2021-11-17 23:45:54 +0100

    Update script-01.ps1

--- Script-01/script-01.ps1
+++ Script-01/script-01.ps1
@@ -1,7 +1,7 @@
 [int]$number1 = read-host -prompt "Enter Your First Number "
 [int]$number2 = read-Host -prompt "Enter Your Second Number "
 $operator = read-host -prompt "Enter Operator Here + , * , / , - "
-
+# Using Switch Instead of if Statemnet
 switch ($operator){
         "+" { $number1 + $number2 }
         "-" { $number1 - $number2 }
