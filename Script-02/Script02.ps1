a96a7afee8e8023255dcd766e2de8e8a9a0be422
Th3Mast3rM1nd <92652606+Th3Mast3rM1nd@users.noreply.github.com>
2021-11-18 20:57:45 +0100

    Update Script02.ps1

--- Script-02/Script02.ps1
+++ Script-02/Script02.ps1
@@ -1,16 +1,16 @@
-[string]$ip = read-host -prompt " Enter Ip to Convert to Binary "
-$splied_ip = $ip -split '\.'
-foreach ( $ips in $splied_ip ) {
-        $binary = [convert]::ToString([int32]$ips,2)
+[string]$ip = read-host -prompt " Enter Ip to Convert It to Binary "
+$split_ip = $ip -split '\.'
+foreach ( $i in $split_ip ) {
+        $binary = [convert]::ToString([int32]$i,2)
         if ($binary.length -lt 8 ) {
                 $number = 8 - $binary.length
                 $zero = "0" * [int]$number
                 $zero += $binary
                 $binary = $zero
-                $binray_array += @($binary)
+                $binary_array += @($binary)
         } else {
-        $binray_array += @($binary)
+        $binary_array += @($binary)
         }
 
 }
-$binray_array -join "."
+$binary_array -join "."
